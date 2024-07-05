71370967b3bad48892a8759e459c092fcfa603a2
Mateo Miño <minora.mateo@gmail.com>
2024-07-05 15:13:33 -0500

    Pagina nueva

---
+++ widgets/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'pantalla1.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navegacion Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: FirstScreen(),
+    );
+  }
+}
---
+++ widgets/lib/pantalla1.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'pantalla2.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primera Pantalla'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Ir a la Segunda Pantalla'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    SecondScreen(data: 'Hola desde la Primera Pantalla'),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
